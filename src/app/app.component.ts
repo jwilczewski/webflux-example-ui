57decbb53dc0ec928874991b3f43010f3805f172
Jakub Wilczewski <jwilczewski@consdata.pl>
2019-05-12 10:59:09 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from './user';
+
+@Component({
+    selector: 'app-root',
+    template: `
+        <div>
+            <div *ngFor="let user of users">{{user.name}} {{user.familyname}}</div>
+        </div>
+    `,
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+    users: User[] = [];
+
+    ngOnInit() {
+        const eventSource = new EventSource('http://localhost:8080/findAll');
+        eventSource.addEventListener('message', (msg) => {
+            const user: User = JSON.parse(msg.data);
+            this.users.push(user);
+        });
+        eventSource.onerror = () => {
+            eventSource.close();
+        };
+    }
+}
